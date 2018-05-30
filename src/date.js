a3d2aa13aa6cb3bfae0c77dc86503a92ddd3f817
Andreas Indal <andreas.indal@iteam.se>
2018-05-30 16:45:35 +0200

    Take null dates into account

--- src/date.js
+++ src/date.js
@@ -1,5 +1,5 @@
 import dayjs from 'dayjs'
 
 const DATE_FORMAT = 'D MMM' // D = day of month, MMM = short month, e.g. jan, apr, dec
-export const formatDateRange = (start, end) => `${formatDate(start)} - ${formatDate(end)}`
-export const formatDate = (date) => dayjs(date).format(DATE_FORMAT)
+export const formatDateRange = (start, end) => start && end ? `${formatDate(start)} - ${formatDate(end)}` : 'N/A'
+export const formatDate = (date) => date ? dayjs(date).format(DATE_FORMAT) : 'N/A'
