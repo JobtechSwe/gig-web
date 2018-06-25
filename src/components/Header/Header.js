6a9b9ae19069fd8d67fd458dda263928a1e34bbd
Andreas Indal <andreas.indal@iteam.se>
2018-06-25 17:02:21 +0200

    fix svg props

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,9 +8,9 @@ import colors from '../../colors'
 
 const Hamburger = styled(({ className }) =>
   <svg className={className} width="28" height="20" viewBox="0 0 28 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <line y1="-1" x2="28" y2="-1" transform="translate(0 2)" stroke="#00005A" stroke-width="2"/>
-    <line y1="-1" x2="28" y2="-1" transform="translate(0 11)" stroke="#00005A" stroke-width="2"/>
-    <line y1="-1" x2="28" y2="-1" transform="translate(0 20)" stroke="#00005A" stroke-width="2"/>
+    <line y1="-1" x2="28" y2="-1" transform="translate(0 2)" stroke="#00005A" strokeWidth="2"/>
+    <line y1="-1" x2="28" y2="-1" transform="translate(0 11)" stroke="#00005A" strokeWidth="2"/>
+    <line y1="-1" x2="28" y2="-1" transform="translate(0 20)" stroke="#00005A" strokeWidth="2"/>
   </svg>
 )`
   margin-top: 0.7em;
