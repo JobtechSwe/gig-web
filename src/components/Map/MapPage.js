a255298b2537861e450d8477833d6fb6f81b27a4
Andreas Indal <andreas.indal@iteam.se>
2018-11-08 16:31:12 +0100

    feat: make it possible to select between jobs on the same location on the map

--- src/components/Map/MapPage.js
+++ src/components/Map/MapPage.js
@@ -9,10 +9,12 @@ import mapStyles from '../../resources/map.json'
 import markerIcon from '../../resources/img/marker.svg'
 import colors from '../../colors'
 
+import Card from '../Card/Card'
+import Body from '../Card/Body'
 import JobCard from '../Jobs/JobCard'
 import Loader from '../Spinner/Loader'
 
-const { MarkerClusterer } = require("react-google-maps/lib/components/addons/MarkerClusterer");
+import { MarkerClusterer } from 'react-google-maps/lib/components/addons/MarkerClusterer'
 
 const CloseButton = styled(({ className, onClick }) => <Link to="#" onClick={onClick} className={className}><i className="i-times"></i></Link>)`
   position: absolute;
@@ -24,7 +26,12 @@ const CloseButton = styled(({ className, onClick }) => <Link to="#" onClick={onC
   text-shadow: 0 0 8px ${colors.gray50};
 `
 
-const Map = withScriptjs(withGoogleMap(({ lat, lng, jobs, onSelectJob, onMapClick }) =>
+const roundCoordinates = (lat, lng) =>
+  [Math.round(lat * 10000) / 10000, Math.round(lng * 10000) / 10000]
+
+const Map = withScriptjs(withGoogleMap(({
+  lat, lng, jobs, onSelectJob, onSelectMultipleJobs, onMapClick
+}) =>
   <GoogleMap
     defaultZoom={10}
     defaultCenter={{ lat, lng }}
@@ -32,14 +39,34 @@ const Map = withScriptjs(withGoogleMap(({ lat, lng, jobs, onSelectJob, onMapClic
     onClick={onMapClick}
   >
     <MarkerClusterer
-      onClick={() => {}}
+      onClick={(markerClusterer) => {
+        if (markerClusterer.getMap().zoom > 20) {
+          const markers = markerClusterer.getMarkers()
+          const locations = markers.map(
+            marker => roundCoordinates(marker.position.lat(), marker.position.lng()))
+
+          const matchingJobs = jobs.filter(
+            job => {
+              const [lat, lng] = roundCoordinates(job.latitude, job.longitude)
+              const match = locations.some(
+                (location) => {
+                  return lat === location[0] && lng === location[1]
+                }
+              )
+              return match
+            })
+
+          onSelectMultipleJobs(matchingJobs)
+        }
+      }}
       averageCenter
       enableRetinaIcons
-      gridSize={60}
+      gridSize={120}
     >
       {
         jobs.map(job =>
           <Marker
+            id={job.id}
             key={job.id}
             options={{ icon: markerIcon }}
             position={{ lat: Number(job.latitude), lng: Number(job.longitude) }}
@@ -79,6 +106,53 @@ const SelectedJobCard = styled(({ className, job }) =>
   }
 `
 
+const MultipleJobsListJob = styled(({ className, job, onSelectJob }) =>
+  <div className={className}>
+    <h3 style={{ fontSize: '16px', marginRight: '4px' }}>{job.title}</h3>
+    <button onClick={() => onSelectJob(job)} style={{
+      color: colors.primary,
+      borderColor: colors.primary,
+      borderWidth: '1px',
+      borderRadius: '3px',
+      background: 'transparent',
+      outline: 0
+    }}>Show</button>
+  </div>
+)`
+  display: flex;
+  justify-content: space-between;
+
+  &:not(:last-child) {
+    padding-bottom: 1em;
+    margin-bottom: 1em;
+    border-bottom: 1px solid ${colors.gray20};
+  }
+`
+
+const MultipleSelectedJobs = styled(({ className, jobs, onSelectJob }) =>
+  <div className={className}>
+    <Card>
+      <Body>
+        <div style={{ maxHeight: '300px', overflowY: 'scroll' }}>
+          {jobs.map(job => <MultipleJobsListJob key={job.id} job={job} onSelectJob={onSelectJob} />)}
+        </div>
+      </Body>
+    </Card>
+  </div>
+)`
+  position: absolute;
+  width: calc(100% - 20px);
+  left: 10px;
+  bottom: 0;
+
+  padding: 20px;
+
+  @media(min-width: 768px) {
+    width: 500px;
+    left: calc(50% - 250px);
+  }
+`
+
 class MapPage extends Component {
   constructor() {
     super()
@@ -88,7 +162,10 @@ class MapPage extends Component {
       position: null,
       error: false,
       jobs: [],
-      selectedJob: undefined
+      showSelectedJob: false,
+      showMultipleJobsList: false,
+      selectedJob: undefined,
+      multipleJobs: [],
     }
   }
 
@@ -105,11 +182,19 @@ class MapPage extends Component {
   }
 
   selectJob(job) {
-    this.setState({ showSelectedJob: true, selectedJob: job })
+    this.setState({ showMultipleJobsList: false, showSelectedJob: true, selectedJob: job })
+  }
+
+  selectMultipleJobs(jobs) {
+    this.setState({ showMultipleJobsList: true, multipleJobs: jobs })
   }
 
   unselectJob() {
     this.setState({ showSelectedJob: false })
+  }
+
+  unselectMultipleJobs() {
+    this.setState({ showMultipleJobsList: false })
   }
 
   render() {
@@ -144,12 +229,28 @@ class MapPage extends Component {
           lng={longitude}
           jobs={this.state.jobs}
           onSelectJob={this.selectJob.bind(this)}
-          onMapClick={this.unselectJob.bind(this)}
+          onSelectMultipleJobs={this.selectMultipleJobs.bind(this)}
+          onMapClick={() => {
+            this.unselectJob()
+            this.unselectMultipleJobs()
+          }}
           googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`}
           loadingElement={<div style={{ position: 'relative', height: `calc(100vh - ${headerHeight}px)` }} />}
           containerElement={<div style={{ position: 'relative', height: `calc(100vh - ${headerHeight}px)` }} />}
           mapElement={<div style={{ position: 'relative', height: `calc(100vh - ${headerHeight}px)` }} />}
         />
+        <CSSTransition
+          in={this.state.showMultipleJobsList}
+          timeout={300}
+          classNames="job-list"
+          unmountOnExit
+          onExited={() => this.setState({ multipleJobs: [] })}
+        >
+          <MultipleSelectedJobs
+            jobs={this.state.multipleJobs}
+            onSelectJob={this.selectJob.bind(this)}
+          />
+        </CSSTransition>
         <CSSTransition
           in={this.state.showSelectedJob}
           timeout={300}
